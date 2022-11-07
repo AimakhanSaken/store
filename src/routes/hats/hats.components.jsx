5b02eb65c06096063ca5f114896cd575090f3676
Aimakhan <aimakhansaken90@gmail.com>
2022-11-07 16:01:55 +0600

    header

---
+++ src/routes/hats/hats.components.jsx
@@ -0,0 +1,10 @@
+
+const Hats = () => {
+    return (
+        <div className="hats-main">
+            <h2>Hats gonna be here</h2>
+        </div>
+    )
+}
+
+export default Hats
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom"
+import mainLogo from '../../images/crown.svg'
+import { Fragment } from "react"
+import './navigation.styles.scss'
+
+
+const Navigation = () => {
+    return (
+      <Fragment>
+        <div className="navigation ">
+            <Link className="logo-container" to='/'>
+                <img className="logo" src={mainLogo}/>
+            </Link>
+            <div className="nav-links-container">
+                <Link className="nav-link" to='/hats'>
+                    SHOP
+                </Link>
+            </div>
+        </div>
+        <Outlet />
+      </Fragment>
+    )
+  }
+
+export default Navigation
