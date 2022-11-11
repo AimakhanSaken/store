d5e38689566f60ae3c6aee7e967c2a2d804b13ca
Aimakhan <aimakhansaken90@gmail.com>
2022-11-11 23:02:14 +0600

    firebase add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./routes/home/home.components";
 import Hats from "./routes/hats/hats.components";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 
 
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />}/> 
         <Route path="hats" element={<Hats />}/>
+        <Route path="sign-in" element={<SignIn />}/>
       </Route>    
     </Routes>
   );
